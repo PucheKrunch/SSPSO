74f9c0e8e1488cfa00c4fdc6a31569520d00b33f
Santiago Benavides <santiago.benavides@alumnos.udg.mx>
2022-02-11 14:15:51 -0600

    CÓMO QUE 150K REGISTROS

---
+++ P02/p02.py
@@ -0,0 +1,61 @@
+import json
+
+data = open('data.txt', 'r', encoding="utf-8").readlines()
+
+states = {
+    "Ciudad de México" : {},
+    "Aguascalientes" : {},
+    "Baja California" : {},
+    "Baja California Sur" : {},
+    "Campeche" : {},
+    "Coahuila de Zaragoza" : {},
+    "Colima" : {},
+    "Chiapas" : {},
+    "Chihuahua" : {},
+    "Durango" : {},
+    "Guanajuato" : {},
+    "Guerrero" : {},
+    "Hidalgo" : {},
+    "Jalisco" : {},
+    "México" : {},
+    "Michoacán de Ocampo" : {},
+    "Morelos" : {},
+    "Nayarit" : {},
+    "Nuevo León" : {},
+    "Oaxaca" : {},
+    "Puebla" : {},
+    "Querétaro" : {},
+    "Quintana Roo" : {},
+    "San Luis Potosí" : {},
+    "Sinaloa" : {},
+    "Sonora" : {},
+    "Tabasco" : {},
+    "Tamaulipas" : {},
+    "Tlaxcala" : {},
+    "Veracruz de Ignacio de la Llave" : {},
+    "Yucatán" : {},
+    "Zacatecas" : {},
+}
+
+for line in data:
+    line = line.strip("\n")
+    line = line.split("|")
+    line.remove("")
+    try:
+        states[line[4]][line[0]].append({
+            "Colonia" : line[1],
+            "Municipio" : line[4] + " " + line[3],
+        })
+    except:
+        states[line[4]][line[0]] = []
+        states[line[4]][line[0]].append({
+            "Colonia" : line[1],
+            "Municipio" : line[4] + " " + line[3],
+        })
+
+
+for n in states:
+    out_file = open(f'{n}.json', 'w')
+    json_object = json.dumps(states[n], indent=4)
+    out_file.write(json_object)
+    out_file.close()
